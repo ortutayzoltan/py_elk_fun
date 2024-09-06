5c68b2672af2ca194c44d2a3a44c8112ad9d252f
Zoltan Zsolt Ortutay <ortutay.zoltan@gmail.com>
2024-09-06 13:53:47 +0200

    Add EQ|QL test

--- esql_test.py
+++ esql_test.py
@@ -5,5 +5,5 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch("http://localhost:9200")
 # Example search query
 
-res = es.search(index="fitnotes", body={"query": {"match_all": {}}})
-print(res['hits']['hits'])
+res = es.esql.query( query="FROM fitnotes | KEEP Date,Weight | LIMIT 10",  format="json")
+print(res['values'])
